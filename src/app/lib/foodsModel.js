c251a0e053a09cc5ed36b22cd9da0f23deccb179
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-10-29 17:21:20 +0530

    crate Food item collection and schema

---
+++ src/app/lib/foodsModel.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const foodModel = new mongoose({
+  item_name: String,
+  item_price: Number,
+  item_path: String,
+  item_description: String,
+  resto_id: mongoose.Schema.Types.ObjectId,
+  item_gen_date: { type: Date, default: Date.now }  // Automatically sets the current date
+});
+
+export const foodSchema = mongoose.model.foods || mongoose.model("foods", foodModel)
