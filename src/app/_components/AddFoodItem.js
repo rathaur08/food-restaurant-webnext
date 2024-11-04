b6063c796250c342067a650bb4fd4252ab788aec
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-11-04 17:17:48 +0530

    Validation for add food items API-.#16

--- src/app/_components/AddFoodItem.js
+++ src/app/_components/AddFoodItem.js
@@ -9,6 +9,7 @@ const AddFoodItem = () => {
     item_path: "",
     item_description: "",
   });
+  const [error, setError] = useState(false);
 
   // ---------- Login Data Handle Input
   const handleChange = (input) => (e) => {
@@ -16,6 +17,13 @@ const AddFoodItem = () => {
   };
 
   const handleAddFoodItem = async () => {
+    if (!addItem.item_name || !addItem.item_price || !addItem.item_path || !addItem.item_description) {
+      setError(true);
+      return false
+    } else {
+      setError(false);
+    }
+
     let resto_id;
     const restaurantData = JSON.parse(localStorage.getItem("restaurantUser"))
     if (restaurantData) {
@@ -51,32 +59,40 @@ const AddFoodItem = () => {
         <h2> Add New Food Item</h2>
         <form>
           <div className="mb-3">
-            <label className="form-label">Item Name</label>
-            <input type="text" className="form-control"
+            <input type="text" className="form-control" placeholder="Item name"
               value={addItem.item_name}
               onChange={handleChange("item_name")}
             />
+            {error && !addItem.item_name && (
+              <span className="text-error">pls enter valid Name</span>
+            )}
           </div>
           <div className="mb-3">
-            <label className="form-label">Price</label>
-            <input type="text" className="form-control"
+            <input type="text" className="form-control" placeholder="Item Price"
               value={addItem.item_price}
               onChange={handleChange("item_price")}
             />
+            {error && !addItem.item_price && (
+              <span className="text-error">pls enter valid Name</span>
+            )}
           </div>
           <div className="mb-3">
-            <label className="form-label">Path</label>
-            <input type="text" className="form-control"
+            <input type="text" className="form-control" placeholder="Enter Item Image URL/Path"
               value={addItem.item_path}
               onChange={handleChange("item_path")}
             />
+            {error && !addItem.item_path && (
+              <span className="text-error">pls enter valid Name</span>
+            )}
           </div>
           <div className="mb-3">
-            <label className="form-label">Description</label>
-            <input type="text" className="form-control"
+            <input type="text" className="form-control" placeholder="Item Description"
               value={addItem.item_description}
               onChange={handleChange("item_description")}
             />
+            {error && !addItem.item_description && (
+              <span className="text-error">pls enter valid Name</span>
+            )}
           </div>
           <button type="submit" onClick={handleAddFoodItem} className="btn btn-primary">
             Submit
