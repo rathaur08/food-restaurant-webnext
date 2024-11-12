4dc065cb4bea6834e2b7756803fce9e92b7f4acb
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-11-12 17:21:54 +0530

    add/Update Home Page Banner and search

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,16 @@ const page = () => {
     <>
       <div>
         <CustomerHeader />
-        <p>JetSetGo Restaurant Website</p>
+        <div className="main-banner">
+          <h2 className="text-white">JetSetGo Food Delivery App</h2>
+          <div className="search-wraper d-flex justify-content-center">
+            <div class="input-group mb-3">
+              <input type="text" class="form-control search-select" placeholder="Select Place" />
+              {/* <span class="input-group-text">Sunny</span> */}
+              <input type="text" class="form-control search-input" placeholder="Enter food or restaurant name" />
+            </div>
+          </div>
+        </div>
         <RestaurantFooter />
       </div>
     </>
