53a9471143899949248cb16347cba864612d7911
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-11-16 15:13:16 +0530

    create UI Restaurants List with API Home page

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,14 @@ import RestaurantFooter from "./_components/RestaurantFooter";
 const page = () => {
 
   const [locations, setLocations] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
   const [selectLocations, selSelectLocations] = useState("");
 
   // console.log(locations);
 
   useEffect(() => {
     getLoadLocations();
+    getRestaurants();
   }, [])
 
   const getLoadLocations = async () => {
@@ -24,6 +26,15 @@ const page = () => {
     }
   }
 
+  const getRestaurants = async () => {
+    let response = await fetch(`http://localhost:3001/api/customer`)
+    response = await response.json();
+    if (response.success) {
+      setRestaurants(response.result)
+    } else {
+      alert("Restaurant not loading")
+    }
+  }
 
   // const handelListItem = (item) => {
   //   // selSelectLocations(item);
@@ -49,6 +60,22 @@ const page = () => {
               <input type="text" className="form-control search-input" placeholder="Enter food or restaurant name" />
             </div>
           </div>
+        </div>
+        <div className="d-flex flex-wrap justify-content-between gap-3 mt-4 mb-4">
+          {
+            restaurants && restaurants.map((item, index) => (
+              <div className="card restaurant-card" key={index}>
+                <div className="card-body">
+                  <h5 className="card-title">{item.res_name}</h5>
+                  <h6 className="card-subtitle mb-2 text-body-secondary">City : {item.city}</h6>
+                  <p className="card-text m-0">Email : {item.email}</p>
+                  <p className="card-text m-0">Number : {item.number}</p>
+                  <h6 className="card-subtitle mb-2 text-body-secondary">Full Address : {item.full_address}</h6>
+                  {/* <h6 className="card-subtitle mb-2 text-body-secondary">Res Date : {item.res_gen_date}</h6> */}
+                </div>
+              </div>
+            ))
+          }
         </div>
         <RestaurantFooter />
       </div>
