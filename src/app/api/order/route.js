17fec952bba4048057065319ae03f808697d4402
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-12-04 17:20:28 +0530

    Create my order API Route
    .#52 Get my order API

--- src/app/api/order/route.js
+++ src/app/api/order/route.js
@@ -1,5 +1,6 @@
 import { connectionStr } from "@/app/lib/db";
 import { orderSchema } from "@/app/lib/ordersModel";
+import { restaurantSchema } from "@/app/lib/restaurantsModel";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
@@ -10,6 +11,27 @@ export async function POST(request) {
   const orderObj = new orderSchema(payload);
   const result = await orderObj.save();
   if (result) {
+    success = true;
+  }
+  return NextResponse.json({ result, success });
+}
+
+export async function GET(request) {
+  const userId = request.nextUrl.searchParams.get("id");
+  let success = false;
+  await mongoose.connect(connectionStr, { useNewUrlParser: true });
+  let result = await orderSchema.find({ order_user_id: userId })
+  if (result) {
+    let restoData = await Promise.all(
+      result.map(async (item) => {
+        let restoInfo = {};
+        restoInfo.data = await restaurantSchema.findOne({ _id: item.order_resto_id })
+        restoInfo.order_total_amount = item.order_total_amount;
+        restoInfo.order_status = item.order_status;
+        return restoInfo;
+      })
+    )
+    result = restoData;
     success = true;
   }
   return NextResponse.json({ result, success });
