37b57f04ac7b01d9d8e26e56ba06fcecda61be09
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-12-09 12:50:32 +0530

    Some Fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,12 +23,12 @@ export default function RootLayout({ children }) {
         <div>{children}</div>
 
         {/* bootstrap@5.0.2 JS link */}
-        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
           integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-          crossOrigin="anonymous"></Script>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+          crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
           integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-          crossOrigin="anonymous"></Script>
+          crossOrigin="anonymous"></script>
 
       </body>
     </html>
